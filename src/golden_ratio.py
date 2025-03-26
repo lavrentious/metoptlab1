a94371aaf412d03aaaf56231d91c306a803a9292
lavrentious <lavrent0@yandex.ru>
2025-03-26 13:28:42 +0300

    refactor: rename to lab2

--- src/golden_ratio.py
+++ src/golden_ratio.py
@@ -3,15 +3,20 @@ from utils import EPS
 
 
 def golden_ratio(fn: Callable[[float], float], a: float, b: float, eps=EPS) -> float:
+    i = 0
+
     def calc(
         a: float, b: float, x1: float | None = None, x2: float | None = None
     ) -> float:
+        nonlocal i
         if b - a < eps:
             return (a + b) / 2
         if x1 is None:
             x1 = a + 0.382 * (b - a)
         if x2 is None:
             x2 = a + 0.618 * (b - a)
+        print(i, a, b, x1, x2, fn(x1), fn(x2), "<" if fn(x1) < fn(x2) else ">")
+        i += 1
         if fn(x1) < fn(x2):
             return calc(a, x2, None, x1)
         else:
--- src/main.py
+++ src/main.py
@@ -15,14 +15,14 @@ def print_result(
     print(f"xm = {xm}, ym = {ym}")
 
 
-print("1.1. Метод дихотомии:")
-print_result(fn, binary_search)
+# print("1.1. Метод дихотомии:")
+# print_result(fn, binary_search)
 
-print("\n1.2. Метод золотого сечения:")
-print_result(fn, golden_ratio)
+# print("\n1.2. Метод золотого сечения:")
+# print_result(fn, golden_ratio)
 
 print("\n1.3. Метод хорд:")
 print_result(fn, lambda f, a, b: chord(f, df, a, b))
 
-print("\n1.4. Метод Ньютона:")
-print_result(fn, lambda f, a, b: newton(f, df, d2f, a, b))
+# print("\n1.4. Метод Ньютона:")
+# print_result(fn, lambda f, a, b: newton(f, df, d2f, a, b))
